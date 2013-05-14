57ddfc47f0b12e62c8d8aaa82c234ba9bef0c5e5
Melissa Wallace <mhk33@cornell.edu>
2013-05-14 15:34:41 +0000

    back out of changes because of white screen of death
    git-svn-id: https://svn.library.cornell.edu/anthrocollections/trunk@7 73057f79-ccda-4e14-b3b2-0f50b96b7522

--- template.php
+++ template.php
@@ -3,17 +3,3 @@
 /**
  * @file template.php
  */
-<?php
-function bootstrap_anthro_preprocess_page(&$variables) {
-  $front_style = path_to_theme() .'/supersized.css';
-
-  if (file_exists($front_style) && $variables['is_front']) {
-    $include_style = $front_style;
-  }
-
-  if (isset($include_style)) {
-    drupal_add_css($include_style, 'theme', 'all', FALSE);
-    $variables['styles'] = drupal_get_css();
-  }
-}
-?>
