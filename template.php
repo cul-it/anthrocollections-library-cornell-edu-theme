75b5dcbcfff581d8788e7971598f6f0ca16a7bd3
Melissa Wallace <mhk33@cornell.edu>
2013-05-14 15:33:42 +0000

    dynamically add stylesheet to home page
    git-svn-id: https://svn.library.cornell.edu/anthrocollections/trunk@5 73057f79-ccda-4e14-b3b2-0f50b96b7522

--- template.php
+++ template.php
@@ -3,3 +3,17 @@
 /**
  * @file template.php
  */
+<?php
+function boostrap_anthro_preprocess_page(&$variables) {
+  $front_style = path_to_theme() .'/supersized.css';
+
+  if (file_exists($front_style) && $variables['is_front']) {
+    $include_style = $front_style;
+  }
+
+  if (isset($include_style)) {
+    drupal_add_css($include_style, 'theme', 'all', FALSE);
+    $variables['styles'] = drupal_get_css();
+  }
+}
+?>
