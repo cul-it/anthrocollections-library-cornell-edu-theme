fe76529f6825c1f2b8f51bef0726be2aa68e7d9a
Melissa Wallace <mhk33@cornell.edu>
2016-05-25 09:51:13 -0400

    add piwik to anthrocollections

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -98,5 +98,21 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+  <!-- Piwik -->
+  <script type="text/javascript">
+    var _paq = _paq || [];
+    _paq.push(["setDomains", ["*.anthrocollections.library.cornell.edu","*.anthrocollections.library.cornell.edu"]]);
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u="//webanalytics.library.cornell.edu/";
+      _paq.push(['setTrackerUrl', u+'piwik.php']);
+      _paq.push(['setSiteId', 545]);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+  </script>
+  <noscript><p><img src="//webanalytics.library.cornell.edu/piwik.php?idsite=545" style="border:0;" alt="" /></p></noscript>
+  <!-- End Piwik Code -->
 </body>
 </html>
